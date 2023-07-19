51ac519ea6620112f49175fbe927573c3c8ac7e5
nazmulrony <nhrony65@gmail.com>
2023-07-19 18:30:23 +0600

    modal started

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={font.className}>
+				<Modal isOpen />
 				<Navbar />
 				{children}
 			</body>
