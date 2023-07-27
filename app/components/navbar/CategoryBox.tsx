eb93d601519d452b1e71fe4772391a16a138dbdf
nazmulrony <nhrony65@gmail.com>
2023-07-27 17:12:02 +0600

    query string added

--- app/components/navbar/CategoryBox.tsx
+++ app/components/navbar/CategoryBox.tsx
@@ -1,6 +1,7 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useCallback } from 'react';
 import { IconType } from 'react-icons';
+import queryString from 'query-string';
 
 interface CategoryBoxProps {
 	icon: IconType;
@@ -18,7 +19,30 @@ export default function CategoryBox({
 	const router = useRouter();
 	const params = useSearchParams();
 
-	const handleClick = useCallback(() => {}, []);
+	//click handler
+	const handleClick = useCallback(() => {
+		let currentQuery = {};
+		if (params) {
+			currentQuery = queryString.parse(params.toString());
+		}
+		const updatedQuery: any = {
+			...currentQuery,
+			category: label,
+		};
+		if (params?.get('category') === label) {
+			delete updatedQuery.category;
+		}
+
+		const url = queryString.stringifyUrl(
+			{
+				url: '/',
+				query: updatedQuery,
+			},
+			{ skipNull: true }
+		);
+		router.push(url);
+	}, [params, label, router]);
+
 	return (
 		<div
 			className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
