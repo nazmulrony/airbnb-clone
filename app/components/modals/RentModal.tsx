d499dede255532172df2d30bd3b1e9612248730c
nazmulrony <nhrony65@gmail.com>
2023-08-01 18:02:47 +0600

    add listing handler added

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,12 +5,15 @@ import { useMemo, useState } from 'react';
 import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
-import { FieldValues, useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
 import ImageUpload from '../inputs/ImageUpload';
 import Input from '../inputs/Input';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 enum STEPS {
 	CATEGORY = 0,
@@ -23,6 +26,7 @@ enum STEPS {
 
 export default function RentModal() {
 	const rentModal = useRentModal();
+	const router = useRouter();
 
 	const [step, setStep] = useState(STEPS.CATEGORY);
 	const [isLoading, setIsLoading] = useState(false);
@@ -79,6 +83,31 @@ export default function RentModal() {
 
 	const onNext = () => {
 		setStep((value) => value + 1);
+	};
+
+	//submit handler function
+
+	const onSubmit: SubmitHandler<FieldValues> = (data) => {
+		if (step !== STEPS.PRICE) {
+			return onNext();
+		}
+		setIsLoading(true);
+
+		axios
+			.post('/api/listings', data)
+			.then(() => {
+				toast.success('Listing Created!');
+				router.refresh();
+				reset();
+				setStep(STEPS.CATEGORY);
+				rentModal.onClose;
+			})
+			.catch((error) => {
+				toast.error(error?.message);
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
 	};
 
 	const actionLabel = useMemo(() => {
@@ -242,8 +271,9 @@ export default function RentModal() {
 			actionLabel={actionLabel}
 			secondaryActionLabel={secondaryActionLabel}
 			secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
-			onSubmit={onNext}
+			onSubmit={handleSubmit(onSubmit)}
 			body={bodyContent}
+			isLoading={isLoading}
 		/>
 	);
 }
