32cebc96bf684a4dc96494bc117ff17fdeb75216
nazmulrony <nhrony65@gmail.com>
2023-08-09 15:18:19 +0600

    all listing info added to details page

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import useCountries from '@/app/hooks/useCountries';
 import { User } from '@prisma/client';
 import { IconType } from 'react-icons';
+import Avatar from '../Avatar';
+import ListingCategory from './ListingCategory';
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import('../Map'), { ssr: false });
 
 interface ListingInfoProps {
 	user: User;
@@ -19,6 +25,43 @@ interface ListingInfoProps {
 	locationValue: string;
 }
 
-export default function ListingInfo({}: ListingInfoProps) {
-	return <div>ListingInfo</div>;
+export default function ListingInfo({
+	user,
+	description,
+	guestCount,
+	roomCount,
+	bathroomCount,
+	category,
+	locationValue,
+}: ListingInfoProps) {
+	const { getByValue } = useCountries();
+	const coordinates = getByValue(locationValue)?.latlng;
+
+	return (
+		<div className="col-span-4 flex flex-col gap-8 ">
+			<div className="flex flex-col gap-2">
+				<div className="text-xl font-semibold flex  items-center gap-2">
+					<p>Hosted by {user?.name}</p>
+					<Avatar src={user?.image} />
+				</div>
+				<div className="flex items-center gap-4 font-light text-neutral-500">
+					<p>{guestCount} guests</p>
+					<p>{roomCount} rooms</p>
+					<p>{bathroomCount} bathrooms</p>
+				</div>
+			</div>
+			<hr />
+			{category && (
+				<ListingCategory
+					icon={category.icon}
+					label={category.label}
+					description={category.description}
+				/>
+			)}
+			<hr />
+			<p className="text-lg font-light text-neutral-500">{description}</p>
+			<hr />
+			<Map center={coordinates} />
+		</div>
+	);
 }
